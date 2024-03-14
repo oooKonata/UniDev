f40e1955953404b5d2f2b82174213dd1f7e98c88
oooKonata <Konata.ppoi@gmail.com>
2024-03-15 00:09:53 +0800

    自定义首页导航栏｜获取胶囊定位，动态配置导航栏高度

---
+++ uni-app-vue-ts/src/pages/index/components/CustomNavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <view
+    class="navbar"
+    :style="{ paddingTop: safeAreaInsets!.top + 'px', height:customNavBarHeight+'px' }"
+  >
+    <text class="title">星和动力</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+  // 获取屏幕安全区域距离(状态栏高度)
+  const { safeAreaInsets } = uni.getSystemInfoSync()
+  // 获取胶囊位置信息
+  const menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+  // 导航栏高度 = 胶囊高度 + (胶囊顶部距离 - 状态栏高度) * 2
+  const customNavBarHeight =
+    menuButtonInfo.height + (menuButtonInfo.top - safeAreaInsets!.top) * 2
+</script>
+
+<style scoped lang="scss">
+  .navbar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: cyan;
+    font-size: 32rpx;
+    color: #333;
+  }
+</style>
--- uni-app-vue-ts/src/pages/index/index.vue
+++ uni-app-vue-ts/src/pages/index/index.vue
@@ -1,38 +1,18 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <CustomNavBar />
   </view>
 </template>
 
 <script setup lang="ts">
+  import CustomNavBar from './components/CustomNavBar.vue'
   import { ref } from 'vue'
   const title = ref('Hello')
 </script>
 
 <style scoped lang="scss">
   .content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .logo {
-      height: 200rpx;
-      width: 200rpx;
-      margin-top: 200rpx;
-      margin-left: auto;
-      margin-right: auto;
-      margin-bottom: 50rpx;
-    }
-    .text-area {
-      display: flex;
-      justify-content: center;
-      .title {
-        font-size: 36rpx;
-        color: #8f8f94;
-      }
-    }
+    background-color: #f5f5f5;
+    height: 100vh;
   }
 </style>
