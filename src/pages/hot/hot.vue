e8752eb9ef04751ff52d66ff163bdfa00700ed8e
oooKonata <Konata.ppoi@gmail.com>
2024-03-26 00:08:52 +0800

    热门推荐详情静态页面

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -23,12 +23,13 @@
           :key="goods"
         >
           <image
-            class="thumb"
+            class="image"
             src="https://yanxuan-item.nosdn.127.net/5e7864647286c7447eeee7f0025f8c11.png"
           />
+          <view class="name ellipsis">不含酒精，使用安心爽肤清洁湿巾</view>
           <view class="price">
-            <text class="symbol">¥</text>
-            <text class="number">29.90</text>
+            <text class="small">¥</text>
+            <text class="price">29.90</text>
           </view>
         </navigator>
       </view>
@@ -41,15 +42,105 @@
 
 <style lang="scss">
 page {
+  height: 100%;
   background-color: #f5f5f5;
-  // height: 100%;
 }
 .viewport {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  position: relative;
+  padding: 177rpx 0 0;
   .cover {
-    margin: 16rpx;
+    padding: 16rpx 16rpx 0;
+    width: 100%;
     height: 225rpx;
+    position: absolute;
+    box-sizing: border-box;
+    top: 0;
     .image {
       border-radius: 16rpx;
+    }
+  }
+  .tabs {
+    z-index: 9;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: #fff;
+    height: 96rpx;
+    margin: 0 48rpx;
+    border-radius: 16rpx;
+    font-size: 28rpx;
+    color: #333;
+    position: relative;
+    margin-bottom: 16rpx;
+    .text {
+      position: relative;
+      line-height: 96rpx;
+    }
+    .active {
+      &::after {
+        content: '';
+        width: 32rpx;
+        height: 4rpx;
+        background-color: #27ba9b;
+        position: absolute;
+        left: 50%;
+        // 左移tab指示器宽度的一半
+        transform: translate(-50%);
+        bottom: 18rpx;
+      }
+    }
+  }
+  .scroll-view {
+    height: 0rpx;
+    flex: 1;
+    margin-bottom: 64rpx;
+    .goods {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      padding: 0 16rpx;
+      .navigator {
+        width: 351rpx;
+        background-color: #fff;
+        border-radius: 16rpx;
+        padding: 24rpx 24rpx 20rpx;
+        margin-bottom: 16rpx;
+        box-sizing: border-box;
+        .image {
+          width: 303rpx;
+          height: 303rpx;
+          border-radius: 16rpx;
+        }
+        .name {
+          font-size: 26rpx;
+          padding-top: 16rpx;
+          /* 块容器中的内容限制为指定的行数 */
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          /* 超过两行裁剪并显示省略号 */
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+        .price {
+          padding-top: 8rpx;
+          color: #cf4444;
+          font-size: 28rpx;
+          .small {
+            font-size: 22rpx;
+          }
+        }
+      }
+    }
+    /* 加载提示文字 */
+    .loading-text {
+      text-align: center;
+      font-size: 26rpx;
+      padding: 16rpx 0 32rpx;
+      color: #999;
     }
   }
 }
